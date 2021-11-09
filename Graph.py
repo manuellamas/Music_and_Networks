3a77e78d9575cfe5bb04792a666d944da21a6cd5
manuellamas <manuelbravolamas@gmail.com>
2021-11-09 18:49:40 +0000

    Created a very basic network from a midi file

--- Graph.py
+++ Graph.py
@@ -1,10 +1,9 @@
 import networkx as nx
 import mido
 
-G = nx.MultiDiGraph() # Creating a directed multigraph
+G = nx.DiGraph() # Creating a directed multigraph
 
 mid = mido.MidiFile("MIDI_files/MIDI_sample.mid", clip = True)
-
 # print(mid)
 
 # Lists all the tracks 'main info'
@@ -16,13 +15,30 @@ first_track = mid.tracks[1] # Starting with 1, not sure what 0 has yet (at least
 
 first_track = first_track[1:-1] # Cut off the first and last entries of the list
 
+
+# Add each note to a list by order of "occurrence". For now I'm just using time of "note_on" of the note
+notes = []
+
 for msg in first_track:
     if msg.type == "note_on" and msg.velocity != 0:
-        print(msg.note)
+        notes.append(msg.note)
 
-# Create a graph that has the above notes as nodes and edges between each of them.
-# Not sure how it works if it has the same "name" which will be the notes themselves, but it should work weel as a multigraph. Meaning it should add only one node per note.
-# What happens when I add a node with the same name (note)? Does it ignore or does it create a double?
-# Maybe I should just add the edges and then create the notes from those edges, I saw that was possible.
+# Count the occurences of pairs of sequential notes
+note_pairs = [] # Elements such as [node1, note2, frequency]
+for i in range(len(notes)-1):
+    pair_found = False
+    for pair in note_pairs:
+        if notes[i] == pair[0] and notes[i+1] == pair[1]: # If the pair has already occurred increased the count
+            pair[2] += 1
+            pair_found = True
+            break
+    if not pair_found: # If that pair occurred yet add it to the list
+        note_pairs.append([notes[i],notes[i+1],1])
 
-# Also, make this a github repo
+# Add edges to the graph from the list of pairs that occurred
+for pair in note_pairs:
+    G.add_weighted_edges_from([(pair[0],pair[1],pair[2])])
+
+
+print(G.in_degree[45])
+print(G.out_degree[45])
