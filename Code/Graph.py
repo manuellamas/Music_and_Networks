3d929c455ea917a13d0f7cdb0248a0c5d4872b32
manuellamas <manuelbravolamas@gmail.com>
2021-12-22 17:26:39 +0000

    Separated MIDI file functions into MIDI_general.py

--- Code/Graph.py
+++ Code/Graph.py
@@ -5,37 +5,7 @@ import os.path
 import Music_Mapping
 import Plotting
 import Graph_metrics
-
-# Separate the info function as a separate file.
-def midi_file_overview(mid_file, filename):
-    """ Exports into a txt file type, number of tracks, and the MIDI Messages themselves """
-    file = open("MIDI_file_info.txt", "w")
-
-    file.write(filename+"\n")
-
-    # Information on the MIDI file type
-    if mid_file.length == ValueError:
-        file.write("MIDI file of type 2, asynchronous")
-        midi_type = 2
-    elif len(mid_file.tracks) != 1:
-        file.write("MIDI file of type 1, synchronous")
-        midi_type = 1
-    else:
-        file.write("MIDI file of type 0, single track")
-        midi_type = 0
-    file.write("\n")
-
-    # Number of tracks
-    if midi_type != 0:
-        file.write("Number of tracks: {}" .format(len(mid_file.tracks)))
-    
-    file.write("\n----------\n\n")
-
-    # Lists all the tracks 'main info'
-    for i, track in enumerate(mid_file.tracks):
-        file.write("{}: {}\n\n" .format(i,track))
-
-    file.close()
+import MIDI_general
 
 
 def graph_display_info(G):
@@ -66,19 +36,6 @@ def graph_display_info(G):
     print("Total sum out-degree: ", total_out_degree)
 
 
-def midi_filename(mid_file):
-    """ A function that takes a path to midi file and returns just the file's name """
-    original_file = mid_file.filename
-    start = 0
-    end = len(original_file) - 1
-    for i , s in enumerate(original_file): # It might make more sense using Regex here
-        if s == "\\": # Catches the last '\\'
-            start = i + 1 # Exactly where the Filename starts
-        elif s == ".":
-            end = i # One index after the Filename ends
-    original_file = original_file[start:end]
-    return original_file
-
 
 # ----- Main ----- #
 if __name__ == "__main__":
@@ -95,8 +52,8 @@ if __name__ == "__main__":
         file_path = sys.argv[-1]
     mid_file = mido.MidiFile(file_path, clip = True)
     # --------------------
-    original_file = midi_filename(mid_file) # Getting just the file name (without the path)
-    midi_file_overview(mid_file, original_file) # Writing basic info of the midi file to a .txt
+    original_file = MIDI_general.midi_filename(mid_file) # Getting just the file name (without the path)
+    MIDI_general.midi_file_overview(mid_file, original_file) # Writing basic info of the midi file to a .txt
 
 
     # Parsing Input - Command
---
+++ Code/MIDI_general.py
@@ -0,0 +1,45 @@
+""" General MIDI functions """
+
+def midi_file_overview(mid_file, filename):
+    """ Exports into a txt file type, number of tracks, and the MIDI Messages themselves """
+    file = open("MIDI_file_info.txt", "w")
+
+    file.write(filename+"\n")
+
+    # Information on the MIDI file type
+    if mid_file.length == ValueError:
+        file.write("MIDI file of type 2, asynchronous")
+        midi_type = 2
+    elif len(mid_file.tracks) != 1:
+        file.write("MIDI file of type 1, synchronous")
+        midi_type = 1
+    else:
+        file.write("MIDI file of type 0, single track")
+        midi_type = 0
+    file.write("\n")
+
+    # Number of tracks
+    if midi_type != 0:
+        file.write("Number of tracks: {}" .format(len(mid_file.tracks)))
+    
+    file.write("\n----------\n\n")
+
+    # Lists all the tracks 'main info'
+    for i, track in enumerate(mid_file.tracks):
+        file.write("{}: {}\n\n" .format(i,track))
+
+    file.close()
+
+
+def midi_filename(mid_file):
+    """ A function that takes a path to midi file and returns just the file's name """
+    original_file = mid_file.filename
+    start = 0
+    end = len(original_file) - 1
+    for i , s in enumerate(original_file): # It might make more sense using Regex here
+        if s == "\\": # Catches the last '\\'
+            start = i + 1 # Exactly where the Filename starts
+        elif s == ".":
+            end = i # One index after the Filename ends
+    original_file = original_file[start:end]
+    return original_file
