dd7b0d6229858f10a7622003433d7fb39e1c5160
manuellamas <manuelbravolamas@gmail.com>
2022-02-05 19:52:02 +0000

    Added Analysis table specific to compare just 2 Artists/Bands

---
+++ Code/ArtistsAnalysis.py
@@ -0,0 +1,85 @@
+import sys
+import config
+
+import os.path
+from os import listdir
+
+import mido
+import networkx as nx
+
+import Music_Mapping
+import MIDI_general
+import Plot.Plotting_Group_Analysis as plt_analysis
+import SongGroupAnalysis as song_analysis
+
+
+if __name__ == "__main__":
+
+    # Directory Target
+    if len(sys.argv) == 1:
+        print("Running at Song Arena\Artist Analysis")
+        files_directory = config.ROOT + "\\SongArena\\Artist_Analysis" # Where the MIDI files to be analyzed are
+
+    elif len(sys.argv) == 2:
+        files_directory = config.ROOT + "\\" + sys.argv[-1] # Where the MIDI files are
+
+    else:
+        print("Too many arguments")
+        exit()
+
+    # Checking if the directory has only two folders, if it has it assumes it's to distinguish each folder as an artist/band
+    list_folders = [f for f in listdir(files_directory) if os.path.isdir(os.path.join(files_directory, f))]
+    if len(list_folders) != 2:
+        print("The chosen directory doesn't have (exactly) 2 folders")
+        exit()
+    else:
+        print("Looking into the folders", list_folders)
+
+
+    # dirs = []
+    # for i in range(len(list_folders)):
+    #     dirs.append(files_directory + "\\" + list_folders[i])
+
+    # Getting midi files for each artist/band
+    list_files = []
+    for i in range(len(list_folders)):
+        list_files.append([f for f in listdir(files_directory + "\\" + list_folders[i]) if (os.path.isfile(os.path.join(files_directory + "\\" + list_folders[i], f)) and f[-3:]) == "mid"])
+
+    # Create the Graphs
+    networks = []
+    labels = [] # To distinguish each artist/band on plots
+    for i in range(len(list_files)):
+        label = list_folders[i]
+        for mid in list_files[i]:
+            mid_file = mido.MidiFile(files_directory + "\\" + list_folders[i] + "\\" + mid, clip = True)
+
+            network, notes = Music_Mapping.graph_note_pairs_weighted(mid_file)
+            filename = MIDI_general.midi_filename(mid_file)
+
+            networks.append([network, mid_file, filename, notes])
+            labels.append(label)
+
+    # Feature List
+    networks_feature_list = []
+    for network, mid_file, filename, notes in networks:
+        time_length = 0
+        try:
+            time_length = mid_file.length
+        except:
+            print("This MIDI file is of type 2 (asynchronous) and so the length can't be computed")
+            print("This song '" + filename + "'should probably be removed, or the analysis done without the length")
+
+        if time_length == 0:
+            networks_feature_list.append(song_analysis.music_data(network, 0))
+        else:
+            networks_feature_list.append(song_analysis.music_data(network, len(notes)/time_length))
+
+
+    # k-means
+    kmean_predictions = song_analysis.kmeans_analysis(networks_feature_list)
+    plt_analysis.clustering_table(networks, kmean_predictions, "k-means", labels)
+
+    # DBSCAN
+    dbscan_predictions = song_analysis.dbscan_analysis(networks_feature_list)
+    print(len(dbscan_predictions))
+    plt_analysis.clustering_table(networks, dbscan_predictions, "DBSCAN", labels)
--- Code/Plot/Plotting_Group_Analysis.py
+++ Code/Plot/Plotting_Group_Analysis.py
@@ -6,31 +6,47 @@ import config
 
 import Graph_metrics
 
-def clustering_table(networks, cluster_predictions, model):
+def clustering_table(networks, cluster_predictions, model, labels = None):
     """ Plots a table with the songs/networks and its cluster resulting of the specified model """
 
     cluster_list = []
-    for i in range(len(networks)):
-        cluster_list.append([networks[i][2], cluster_predictions[i]]) # filename
+    if labels is None:
+        for i in range(len(networks)):
+            cluster_list.append([networks[i][2], cluster_predictions[i]]) # filename
+    else:
+        for i in range(len(networks)):
+            cluster_list.append([networks[i][2], cluster_predictions[i], labels[i]]) # filename, labels
+
 
     fig, ax = plt.subplots()
     fig.patch.set_visible(False) # Removing 'background'
     ax.axis("off") # Hide axes
 
-    # color_list = ["b","r","g","y","greenyellow"]
     color_list = ["#ffffb3", "#ff803e", "#ffff68", "#ffa6bd", "#ffc100", "#ffcea2", "#ff8170"]
     # color_list = ["#ffffb300", "#ff803e75", "#ffff6800", "#ffa6bdd7", "#ffc10020", "#ffcea262", "#ff817066"]
     colors = []
     for i in range(len(cluster_predictions)):
         chosen_color = color_list[cluster_predictions[i]]
-        colors.append([chosen_color, chosen_color])
+        if labels is None:
+            colors.append([chosen_color, chosen_color])
+        else:
+            colors.append([chosen_color, chosen_color, chosen_color])
 
-    columns = ["Song", "Cluster"]
+    if labels is None:
+        columns = ["Song", "Cluster"]
+    else:
+        columns = ["Song", "Cluster", "Artist/Band"]
+
     rows = [] #, rowLabels = rows
-    ax.table(cellText = cluster_list, colLabels = columns, loc = "center", cellLoc = "center", cellColours=colors)
+    tab = ax.table(cellText = cluster_list, colLabels = columns, loc = "center", cellLoc = "center", cellColours=colors)
+
+    tab.auto_set_font_size(False) # Makes font size not automatic and instead choose a font size below
+    tab.set_fontsize(8)
+    tab.auto_set_column_width(col=list(range(len(columns)))) # Sets Column width automatically
+
     fig.tight_layout()
 
-    plt.savefig(config.ROOT + "\\Plots\\Single\\"+ model +"_clustering.png")
+    plt.savefig(config.ROOT + "\\Plots\\SongGroupAnalysis\\"+ model +"_clustering.png")
 
 
 
