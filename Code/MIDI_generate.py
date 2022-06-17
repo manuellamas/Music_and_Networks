c5f2652891a8c67a39fce3194ea282c2e0303256
manuellamas <manuelbravolamas@gmail.com>
2022-06-17 19:38:04 +0100

    Added several midi generators

--- Code/MIDI_generate.py
+++ Code/MIDI_generate.py
@@ -1,9 +1,10 @@
 """ Generating Synthetic MIDI files with specific characteristics to test our analysis """
 
 import mido
+import random
 
 
-def midi_synthetic_1():
+def midi_synthetic(midi_generator, **args):
     """ Generate a MIDI file for testing """
 
     # New midi file
@@ -24,7 +25,6 @@ def midi_synthetic_1():
 
 
 
-
     # Specifying the track name
     track_name_message = mido.MetaMessage("track_name", name = "Ragtime Piano")
     track.append(track_name_message)
@@ -35,12 +35,8 @@ def midi_synthetic_1():
 
 
 
-
-    ### Specifying what to generate
-    # title = midi_repeat_up(track)
-    # title = midi_straight_up_rising_octave(track)
-    title = midi_straight_up_fixed_octave(track)
-
+    ### Specifying what to generate (and getting the title)
+    title = midi_generator(track, **args) # This is adding messages to the main track via an inputed function
 
 
 
@@ -51,50 +47,257 @@ def midi_synthetic_1():
     return
 
 
-def midi_repeat_up(track):
-    """ Repeating the same note n times until going through the entire octave """
-    for i in range(0, 11+1):
-        for j in range(5):
+
+
+
+##################################################
+#------------------------------------------------#
+##################################################
+# Generators
+
+# Choose an even number of REPETITIONS (For, for example, the up down up down sequences)
+REPETITIONS = 6
+
+
+
+
+
+
+def midi_fixed_note(track, note = 1):
+    """ Keeps playing the same note """
+    
+    repeat(track, note, times = REPETITIONS*12)
+
+    return "fixed_note" + "_" + str(note).zfill(3) # zfill pads the number to have three digits
+
+
+
+def midi_repeat_aabb(track, starting_note = 0, last_note = 11, up = True):
+    """
+    Repeating the same note n times until going through the entire octave
+    DO DO ... DO RE RE ... RE ...
+    Or the same but starting on the highest note
+    """
+
+    if up:
+        range_interval = range(starting_note, last_note + 1)
+    else:
+        range_interval = reversed(range(starting_note, last_note + 1))
+
+    for i in range_interval:
+        repeat(track, i, REPETITIONS)
+
+    if up:
+        return "repeat_aabb_up"
+    else:
+        return "repeat_aabb_down"
+
+
+
+#########################
+## Straight Up or Down ##
+#########################
+
+def midi_straight_rising_octave(track, starting_note = 0, last_note = (12*REPETITIONS - 1), up = True):
+    """
+    Do Re Mi ... - going up to different octaves
+    OR
+    ... Mi Re Do - going down to different octaves
+    """
+
+    straight(track, starting_note = starting_note, last_note = last_note, up = up)
+
+    if up:
+        return "straight_rising_octave_up"
+    else:
+        return "straight_rising_octave_down"
+
+
+
+def midi_straight_fixed_octave(track, up = True):
+    """ Do Re Mi ... Do - on a loop up at a fixed octave """
+
+    for j in range(REPETITIONS):
+        straight(track, starting_note = 0, last_note = 11, up = up)
+    
+    if up:
+        return "straight_fixed_octave_up"
+    else:
+        return "straight_fixed_octave_down"
+
+
+
+
+#######################
+## Peaks and Valleys ##
+#######################
+
+def midi_peak_fixed_octave(track, starting_note = 0, highest_note = 11):
+    """
+    Straight up and down on the same octave
+    Do Re Mi ... Do - on a loop down at a fixed octave
+    """
+
+    for j in range(REPETITIONS):
+        peak(track, starting_note = starting_note, highest_note = highest_note)
+
+    return "peak_fixed_octave"
+
+
+
+def midi_small_large_peaks_constant(track, starting_note = 0, small_peak_highest = 5, large_peak_highest = 11):
+    """ 
+    Small peak, large peak, on a loop
+    """
+
+    for j in range(REPETITIONS):
+        peak(track, starting_note, small_peak_highest) # Small Peak
+        peak(track, starting_note, large_peak_highest) # Large Peak
+
+    return "peaks_small_large_constant"
+
+
+
+def midi_small_large_peaks_rising(track):
+    """
+    Small peak, large peak ending above the initial point
+    Then loop
+    Making it have a up tendency
+    """
+
+    for j in range(REPETITIONS):
+        peak(0,5) # Small Peak
+        peak(0,11) # Large Peak
+
+    return "peaks_small_large_rising"
+
+
+
+############
+## Random ##
+############
+
+random.seed(42)
+
+def midi_random_fixed_octave(track, starting_note = 0, last_note = 11):
+    """
+    Random notes within a fixed octave
+    It's only on a fixed octave depending on the values of starting_note and last_note
+    """
+    for i in range(REPETITIONS*12):
+            random_note = random.randint(starting_note, last_note) # Random number between starting_note and last_note (including)
+
+            message_on = mido.Message('note_on', note = random_note, velocity = 50, time = 20)
+            track.append(message_on)
+
+            message_off = mido.Message('note_off', note = random_note, velocity = 50, time = 300)
+            track.append(message_off)
+
+    return "random_fixed_octave"
+
+
+
+def midi_fully_random(track):
+    """ Fully random notes """
+    for i in range(REPETITIONS*12):
+            random_note = random.randint(0, 127) # Random number between 0 and 127 (including)
+
+            message_on = mido.Message('note_on', note = random_note, velocity = 50, time = 20)
+            track.append(message_on)
+
+            message_off = mido.Message('note_off', note = random_note, velocity = 50, time = 300)
+            track.append(message_off)
+
+    return "random_fully"
+
+
+
+
+
+##################################################
+#------------------------------------------------#
+##################################################
+# Supporting Functions
+
+def straight(track, starting_note = 0, last_note = 11, up = True):
+    """ Steadily go up (or down) from starting_note until last_note """
+
+    if up:
+        range_interval = range(starting_note, last_note + 1)
+    else:
+        range_interval = reversed(range(starting_note, last_note + 1))
+
+    for i in range_interval:
             message_on = mido.Message('note_on', note = i, velocity = 50, time = 20)
             track.append(message_on)
 
             message_off = mido.Message('note_off', note = i, velocity = 50, time = 300)
             track.append(message_off)
 
-
-    return "repeat_up"
-
+    return
 
 
-def midi_straight_up_rising_octave(track):
-    """ Do Re Mi ... - going up to different octaves """
-    for i in range(0, (12*5 - 1) + 1):
+
+def repeat(track, note_to_repeat = 0, times = 1):
+    """ Repeats the same note a specific number of times """
+
+    for i in range(times):
+        message_on = mido.Message('note_on', note = note_to_repeat, velocity = 50, time = 20)
+        track.append(message_on)
+
+        message_off = mido.Message('note_off', note = note_to_repeat, velocity = 50, time = 300)
+        track.append(message_off)
+
+    return
+
+
+
+def peak(track, starting_note = 0, highest_note = 11):
+    """ Notes going up from the starting_note until reaching the highest_note, then going down until reaching starting_note """
+
+    for i in range(starting_note, highest_note + 1): # Going up
         message_on = mido.Message('note_on', note = i, velocity = 50, time = 20)
         track.append(message_on)
 
         message_off = mido.Message('note_off', note = i, velocity = 50, time = 300)
         track.append(message_off)
-    
-    return "straight_up_rising_octave"
+    for i in reversed(range(starting_note, (highest_note - 1) + 1)): # Going down - It's (highest_note - 1) so that it doesn't repeat the "peak" note
+        message_on = mido.Message('note_on', note = i, velocity = 50, time = 20)
+        track.append(message_on)
 
+        message_off = mido.Message('note_off', note = i, velocity = 50, time = 300)
+        track.append(message_off)
 
-
-
-
-def midi_straight_up_fixed_octave(track):
-    """ Do Re Mi ... Do - on a loop up at a fixed octave """
-
-    for j in range(5):
-        for i in range(0, 11+1):
-            message_on = mido.Message('note_on', note = i, velocity = 50, time = 20)
-            track.append(message_on)
-
-            message_off = mido.Message('note_off', note = i, velocity = 50, time = 300)
-            track.append(message_off)
-    
-    return "straight_up_fixed_octave"
+    return
 
 
 
 if __name__ == "__main__":
-    midi_synthetic_1()
+    midi_synthetic(midi_fixed_note)
+
+    midi_synthetic(midi_repeat_aabb, up = True)
+    midi_synthetic(midi_repeat_aabb, up = False)
+
+    midi_synthetic(midi_straight_rising_octave, up = True)
+    midi_synthetic(midi_straight_rising_octave, up = False)
+
+    midi_synthetic(midi_straight_fixed_octave, up = True)
+    midi_synthetic(midi_straight_fixed_octave, up = False)
+
+
+    # Peaks and Valleys
+    midi_synthetic(midi_peak_fixed_octave)
+    midi_synthetic(midi_small_large_peaks_constant)
+
+
+
+    # Random
+    midi_synthetic(midi_random_fixed_octave)
+    midi_synthetic(midi_fully_random)
+
+
+
+
+
+
+
