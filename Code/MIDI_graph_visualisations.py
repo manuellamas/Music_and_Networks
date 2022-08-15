8deb6b914101e0a1f900dadf225eb1f2310b3e07
manuellamas <manuelbravolamas@gmail.com>
2022-08-15 19:33:47 +0100

    Edge color based on weight (proportional to the graph's max and min)

--- Code/MIDI_graph_visualisations.py
+++ Code/MIDI_graph_visualisations.py
@@ -44,46 +44,78 @@ def create_graph_vis(G, filename):
     # Do something similar to edges' thickness, or maybe color using a color pallete that indicates it
 
 
-    # for edge in G.edges(data = "weight"):
-    #     # nx.draw_networkx_edges(G, pos, edgelist=[edge], width=edge[2], edge_color = "red")
-    #     nx.draw_networkx_edges(G, pos, edgelist=[edge], edge_color = "red")
+    # https://www.color-hex.com/color-palette/2539
+    # Bright to Dark
+    red_shades = ["#ffbaba","#ff7b7b","#ff5252","#ff0000","#a70000"]
+    num_edge_divisions = len(red_shades)
 
 
-    ## Adjust this for edge's weight
-    # min_value = list(G.nodes)[0]
-    # max_value = list(G.nodes)[0]
-    # print(G.nodes(data = True))
-    # print(G.nodes(data = "weight"))
-    # for node in G:
-    #     print(node)
-    #     print(int(node))
-    #     max_value = max(max_value, node)
-    #     min_value = min(min_value, node)
+    # Dictionary that maps edge tuple to its weight
+    edge_weights = nx.get_edge_attributes(G, "weight") # https://networkx.org/documentation/stable/reference/generated/networkx.classes.function.get_edge_attributes.html
+
+    # [Shades of Red Color Palette](https://www.color-hex.com/color-palette/2539)
+    # [get_edge_attributes — NetworkX 2.8.5 documentation](https://networkx.org/documentation/stable/reference/generated/networkx.classes.function.get_edge_attributes.html)
 
 
+    ## Creating the list that will allow "mapping" each edge, given its weight, to a specific color
+    # Initializing min and max value as the weight of the "first" edge when considering the list of edges
+    min_edge_weight_value = edge_weights[list(G.edges)[0]]
+    max_edge_weight_value = edge_weights[list(G.edges)[0]]
+
+    for edge in G.edges:
+        edge_weight = edge_weights[edge]
+        min_edge_weight_value = min(min_edge_weight_value, edge_weight)
+        max_edge_weight_value = max(max_edge_weight_value, edge_weight)
+
+    edge_interval_length = (max_edge_weight_value - min_edge_weight_value) / num_edge_divisions # The length of each division
+
+    edge_weight_divisons = [] # The list to be used to check to which division/color the edge belongs given its weight
+    
+    edge_weight_divisons = [min_edge_weight_value + i*edge_interval_length for i in range(num_edge_divisions - 1)]
+    edge_weight_divisons.append(max_edge_weight_value)
+
+
+    ## TESTING
+    print(edge_weight_divisons)
+    for i in range(num_edge_divisions):
+        print(min_edge_weight_value + i*edge_interval_length, min_edge_weight_value + (i+1)*edge_interval_length)
+        # print(min+i*ui,min+(i+1)*ui)
+    ## TESTING
+
+
+    ## -----
+
+    for edge in G.edges:
+        # Finding the color given its weight. There are num_edge_divisions colors. S
+        edge_color = -1
+        edge_weight = edge_weights[edge]
+        for i in range(len(edge_weight_divisons)):
+            if edge_weight < edge_weight_divisons[i]:
+                edge_color = i - 1
+                break
+        # nx.draw_networkx_edges(G, pos, edgelist=[edge], width=edge[2], edge_color = "red")
+        nx.draw_networkx_edges(G, pos, edgelist=[edge], edge_color = red_shades[edge_color])
 
 
 
     ### Setting Node's size as its relative indegree value
 
     ## Get maximum and minimum value of node degree
-
     # Initializing both max and min values as the graph's "first" node's in-degree. "First" node of the list created from the directory (which represents the Graph and holds no order)
-    min_value = G.in_degree(list(G.nodes)[0])
-    max_value = G.in_degree(list(G.nodes)[0])
-    for node in G:
-        max_value = max(max_value, G.in_degree(node))
-        min_value = min(min_value, G.in_degree(node))
+    min_node_indegree_value = G.in_degree(list(G.nodes)[0])
+    max_node_indegree_value = G.in_degree(list(G.nodes)[0])
+    for node in G.nodes:
+        min_node_indegree_value = min(min_node_indegree_value, G.in_degree(node))
+        max_node_indegree_value = max(max_node_indegree_value, G.in_degree(node))
 
 
-    # Create a function that takes those two as input and a third which is the node to be changed's in-degree
 
     node_size_min = 100
     node_size_max = 500
 
     for node in G.nodes:
         # Get Node's indegree as a relative value (mapping from [min_value, max_value] to [node_size_min, node_size_max])
-        relative_value = node_size_min + (node_size_max - node_size_min)/(max_value - min_value)*(node - min_value)
+        relative_value = node_size_min + (node_size_max - node_size_min)/(max_node_indegree_value - min_node_indegree_value)*(node - min_node_indegree_value)
 
         nx.draw_networkx_nodes(G, pos, nodelist = [node], node_size = relative_value)
 
